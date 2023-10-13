e6d49bbf49841eec3196106beabe6f51291b8174
Chris Banford <chris@bsoft.ch>
2023-10-13 16:53:45 +0200

    Lots and lots of image bg on Nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
+import prefetch from '@astrojs/prefetch';
 
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [
+    vue(), 
+    tailwind(), 
+    prefetch()
+  ]
 });
