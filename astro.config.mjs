decc28a4207c710abc533969aefdf44b826e23f9
Chris Banford <chris@bsoft.ch>
2023-10-15 20:55:36 +0200

    Added Sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import prefetch from '@astrojs/prefetch';
-
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    vue(), 
-    tailwind(), 
-    prefetch()
-  ]
+  integrations: [vue(), tailwind(), prefetch(), sitemap()]
 });
