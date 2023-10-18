1c68f3e4d54cf47a6b577929143f1d12633acd41
Chris Banford <chris@bsoft.ch>
2023-10-18 13:26:17 +0200

    Netlify all good.

--- src/components/booking/Pages.vue
+++ src/components/booking/Pages.vue
@@ -6,18 +6,18 @@
     <button @click="myComponent ='PageFlight'">Page Flight</button>
 
     <!-- Previous Button -->
-    <a href="#"
+    <a href="#" @click="myComponent ='PageDate'"
         class="flex items-center justify-center px-3 h-8 mr-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
         <svg class="w-3.5 h-3.5 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
             viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M13 5H1m0 0 4 4M1 5l4-4" />
         </svg>
-        Previous
+        Page Date
     </a>
-    <a href="#"
+    <a href="#" @click="myComponent ='PageFlight'"
         class="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-        Next
+        Page Flight
         <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
             viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
