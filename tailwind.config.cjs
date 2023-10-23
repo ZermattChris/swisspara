80f186fab4b45c6c0a139e77f26a74418a919e75
Chris Banford <chris@bsoft.ch>
2023-10-23 14:52:58 +0200

    Reverted to working (hoping) commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
 	],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		require('flowbite/plugin')
+		
 	],
 }
