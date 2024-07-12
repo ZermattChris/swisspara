e7dbe849cf089e3a5f02f701361839fd857b2d6a
Chris Banford <chris@bsoft.ch>
2024-07-12 22:26:41 +0200

    Added "change dates icon tip" to 1. Get Started -- dates.

--- src/components/booking/Date.vue
+++ src/components/booking/Date.vue
@@ -170,7 +170,7 @@
 
       <input type="input" name="arriveDateInput" id="arriveDateInput" :value="displayDate(arriveDate)" readonly
         @click="onArriveDateInputClick('arriveDateInput', $event)" placeholder="Please enter your Arrival Date..."
-        :class="{ 'bg-lime-100': arriveDate }"
+        :class="{ 'bg-lime-50': arriveDate }"
         class="inline-block  rounded-md border-0 
           ml-2 py-2 px-2 
           w-72 sm:w-80
@@ -207,7 +207,7 @@
 
       <input type="input" name="departDateInput" id="departDateInput" :value="displayDate(departDate)" readonly
         @click="onDepartDateInputClick('departDateInput', $event)" placeholder="Please enter your Departure date..."
-        :class="{ 'bg-lime-100': departDate }"
+        :class="{ 'bg-lime-50': departDate }"
         class="inline-block  rounded-md border-0 
           ml-2 py-2 px-2 
           w-72 sm:w-80
@@ -224,6 +224,14 @@
       <span class="bg-yellow-200 text-orange-800 px-1 py-0.5 font-bold">{{ getLengthStayInZermatt }}</span>
       {{ getLengthStayInZermatt === 1 ? 'day' : 'days' }} in Zermatt.
     </div>
+
+    <div class="text-sm  mt-4 pl-">
+      <svg class="w-6 h-6 inline-block relative -top-0.5  text-orange-700" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M9.97308 18H14.0269C14.1589 16.7984 14.7721 15.8065 15.7676 14.7226C15.8797 14.6006 16.5988 13.8564 16.6841 13.7501C17.5318 12.6931 18 11.385 18 10C18 6.68629 15.3137 4 12 4C8.68629 4 6 6.68629 6 10C6 11.3843 6.46774 12.6917 7.31462 13.7484C7.40004 13.855 8.12081 14.6012 8.23154 14.7218C9.22766 15.8064 9.84103 16.7984 9.97308 18ZM14 20H10V21H14V20ZM5.75395 14.9992C4.65645 13.6297 4 11.8915 4 10C4 5.58172 7.58172 2 12 2C16.4183 2 20 5.58172 20 10C20 11.8925 19.3428 13.6315 18.2443 15.0014C17.624 15.7748 16 17 16 18.5V21C16 22.1046 15.1046 23 14 23H10C8.89543 23 8 22.1046 8 21V18.5C8 17 6.37458 15.7736 5.75395 14.9992ZM13 10.0048H15.5L11 16.0048V12.0048H8.5L13 6V10.0048Z"></path>
+      </svg>
+      TIP: It's easy to change your flight dates later if needed.
+    </div>
+
 
   </div>
 
