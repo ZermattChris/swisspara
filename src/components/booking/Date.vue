027f8a4e14b6942b6788c44c2045e0d28e1f11d5
Chris Banford <chris@bsoft.ch>
2024-07-15 08:50:27 +0200

    ...

--- src/components/booking/Date.vue
+++ src/components/booking/Date.vue
@@ -177,10 +177,9 @@
         @click="onArriveDateInputClick('arriveDateInput', $event)" placeholder="Please enter your Arrival Date..."
         :class="{ 'bg-indigo-50/50': arriveDate }"
         class="inline-block  rounded-md border-0 
-          ml-2 py-2 px-2 
+          ml-2 py-2 pl-12 pr-2 
           w-72 sm:w-80
           overflow-clip
-          text-center
           cursor-pointer
         bg-gray-100
           text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 " />
