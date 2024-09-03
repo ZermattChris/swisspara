c192fe643891bfe6c56e86888e7d625e6081025d
Chris Banford <chris@bsoft.ch>
2024-09-03 19:25:31 +0200

    Looking for API path setting to use Stripe Test Mode from Netlify.

--- src/components/booking/Payment.vue
+++ src/components/booking/Payment.vue
@@ -480,9 +480,12 @@ export default {
       // Local, Staging or LIVE path here.
       let host = new URL(document.location).hostname
       let path = 'http://spzadmin.local:88/api/v1/stripe/setup'   // Local or Staging.
+      this.stripeDevMessages += '• This is the API Server path being set (check on Netlify): ' + path + ' </br>'
       if (host == 'localhost') {
         this.stripeDevMessages += '• On Local Dev. Using the Sail API path: ' + path + ' </br>'
       } else if (host == 'swisspara.netlify.app') {
+        // TODO: This is why the staging on Netlify is failing. Need to set a flag to the API 
+        //       to use Stripe Test Mode. 
         this.stripeDevMessages += '• TODO!!!! On Netlify Staging. Using Live API path with Dev flag: ' + path + ' </br>'
       } else {
         // Live Stripe calls.
