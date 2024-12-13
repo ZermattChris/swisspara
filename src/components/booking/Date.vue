d0c0a9eb0f6b29e33117e8ef83e146fbd85f5cbf
Chris Banford <chris@bsoft.ch>
2024-12-13 13:02:40 +0100

    ...

--- src/components/booking/Date.vue
+++ src/components/booking/Date.vue
@@ -814,6 +814,13 @@ export default {
     content: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white"><path d="M14 8.94737L22 14V16L14 13.4737V18.8333L17 20.5V22L12.5 21L8 22V20.5L11 18.8333V13.4737L3 16V14L11 8.94737V3.5C11 2.67157 11.6716 2 12.5 2C13.3284 2 14 2.67157 14 3.5V8.94737Z"></path></svg>');
     filter: drop-shadow(0px 0px 2px rgba(0, 0, 0, 0.7));
   }
+  .dp__active_date:after {
+    content: 'FLY';
+    position: absolute;
+    bottom: -0.35em;
+    font-size: 0.7em;
+    color: rgba(190, 242, 100, 1.0);
+  }
 
 .dpMenuArrive {
   border: 1px solid rgba(76, 187, 23, 1);
